2fcf886265074b21aba9f1a8e9d7903a76a310c2
Alicia Ramos <ramosam@my.lanecc.edu>
2019-10-23 00:46:15 -0700

    added public

--- app/src/main/java/com/example/piggame/PreferencesActivity.java
+++ app/src/main/java/com/example/piggame/PreferencesActivity.java
@@ -3,12 +3,12 @@ package com.example.piggame;
 import android.app.Activity;
 import android.os.Bundle;
 
-class PreferencesActivity extends Activity {
+public class PreferencesActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Display fragment as main content
+
         setContentView(R.layout.activity_preferences);
 
     }
